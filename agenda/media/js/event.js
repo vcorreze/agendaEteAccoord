bfd9e29596195e84638b0be7cd929532500d1104
Sylvain Boureliou <sylvain.boureliou@gmail.com>
2016-06-06 11:34:36 +0200

    TinyMCE uses now fr_FR language

--- agenda/media/js/event.js
+++ agenda/media/js/event.js
@@ -13,7 +13,8 @@
         'save table contextmenu directionality emoticons paste textcolor'
       ],
       content_css: 'css/content.css',
-      toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons'
+      toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons',
+      language: 'fr_FR'
    });
 
 })();
